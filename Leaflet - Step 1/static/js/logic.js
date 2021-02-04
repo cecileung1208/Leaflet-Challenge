8fe0a73bdd2cd64f8b641ca4e2621cd6c0b0315b
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2021-02-03 19:41:31 -0500

    add color function

--- Leaflet - Step 1/static/js/logic.js
+++ Leaflet - Step 1/static/js/logic.js
@@ -14,6 +14,20 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+function getColor(d) {
+  if (d < 10)
+    return "green";
+  else if (d < 30)
+    return "yellow";
+  else if (d < 50)
+    return "pink";
+  else if (d < 70)
+    return "blue";
+  else if (d<90)
+    return "orange";
+  else
+    return "red"
+}
 
 // Create a variable for the geojson
 var queryURL =  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -28,10 +42,16 @@ d3.json(queryURL, function(data) {
   for(var i=0; i<features.length; i++){
   var location = features[i].geometry.coordinates
   var magnitude = features[i].properties.mag
-
+  console.log(location[2])
 
   if (location) {
-    L.marker([location[1], location[0]]).addTo(myMap);
+    L.circle([location[1], location[0]],{
+      fillOpacity: 0.75,
+      color: "black",
+      fillColor: getColor([location[2]]),
+    // Adjust radius
+    radius: magnitude * 20000
+    }).addTo(myMap);
     };
   }
 
