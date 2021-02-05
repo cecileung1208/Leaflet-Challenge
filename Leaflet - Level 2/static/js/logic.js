acb61c3e8d1cd0004a05be77ec91abc9205d7183
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2021-02-05 17:39:51 -0500

    added base layers to map

--- Leaflet - Level 2/static/js/logic.js
+++ Leaflet - Level 2/static/js/logic.js
@@ -1,18 +1,53 @@
-// Create a map object
-var myMap = L.map("mapid", {
-  center: [35.222, -101.8313],
-  zoom: 5
-});
 
 // Add a light tile layer and adding it to myMap
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+
+var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/light-v10",
   accessToken: API_KEY
-}).addTo(myMap);
+})
+
+var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/satellite-v9",
+  accessToken: API_KEY
+});
+
+var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "outdoors-v11",
+  accessToken: API_KEY
+});
+
+// Create a baseMaps object
+var baseMaps = {
+  "Satellite": satelliteMap,
+  "Grayscale": lightMap,
+  "Outdoors": outdoorsMap
+};
+
+// Create an overlay object
+// var overlayMaps ={
+//   "Earthquakes": geoLayer,
+// };
+
+// Create a map object
+var myMap = L.map("mapid", {
+  center: [35.222, -101.8313],
+  zoom: 5,
+  layers: [satelliteMap]
+});
+
+
+
+L.control.layers(baseMaps).addTo(myMap);
 
 
 
