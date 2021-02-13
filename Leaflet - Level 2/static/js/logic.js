613f45afe29ed6d5ad3c7d68e356a87b79fa4f2c
cecileung1208 <71020959+cecileung1208@users.noreply.github.com>
2021-02-13 14:17:28 -0500

    update overlay & basemaps

--- Leaflet - Level 2/static/js/logic.js
+++ Leaflet - Level 2/static/js/logic.js
@@ -1,112 +1,41 @@
-
-// Setting map layer style variables 
-
-// Satellite Map Style
-var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/satellite-v9",
-  accessToken: API_KEY
-});
-
-// Gray Scale Style
-var grayscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/light-v10",
-  accessToken: API_KEY
-})
-
-// Outdoors Style
-var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "outdoors-v11",
-  accessToken: API_KEY
-});
-
-// Create a baseMaps object
-var baseMaps = {
-  "Satellite": satelliteMap,
-  "Grayscale": grayscaleMap,
-  "Outdoors": outdoorsMap
-};
-
-// Create an overlay object
-var overlayMaps ={
-  "Earthquakes": earthquake,
-  "Tectonics": grayscaleMap
-};
-
-// Create a map object
-var myMap = L.map("mapid", {
-  center: [35.222, -101.8313],
-  zoom: 5,
-  layers: [satelliteMap]
-});
-
-
-
-L.control.layers(baseMaps, overlayMaps).addTo(myMap);
-
-
-
-// Create a variable for the geojson url
+// Create a variables for the earthquake and plates URL
 var queryURL =  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var plateURL = "https://github.com/fraxen/tectonicplates/blob/master/GeoJSON/PB2002_boundaries.json";
 
+
+//EARTHQUAKE OVERLAYER
+
+function markerSize(magnitude) {
+  return magnitude * 4;
+};
+
+//Create new layer for the earthquake markers
+var earthquake = new L.LayerGroup();
 
 // Retrieve data from the query url
-d3.json(queryURL, function(json) {
-
+d3.json(queryURL, function (geoJson) {
 
 // Use geoJson to set conditions based on the json data
+  L.geoJSON(geoJson.features, {
 
-var earthquake = L.layerGroup().addTo(map);
- L.geoJson(json, {
+    // Create function to identify earthquake location by using the latlng method
+    pointToLayer: function (feature, latlng) {
+      // Put the circle markers based on the latlng location
+      return L.circleMarker(latlng, { radius: markerSize(feature.properties.mag) });
+        },
 
-    // Set function to retrieve depth data
-    style: function(feature) {
-
-      // Set variable for depth to locate where the data is 
-      var depth = feature.geometry.coordinates[2];
-
-      // Conditional Statement for color scale vs depth
-      // For high depth, circles will be red and low depth, circles will be green
-      
-      if (depth >= 90.0) {
-        return {
-          fillColor: "#FF3333",
-        }; 
-      }
-      else if (depth > 70.0) {
-        return {
-          fillColor: "#FF9966"
-        };
-      } else if (depth > 50) {
+        // Create styles for the circle
+        style: function (geoJsonFeature) {
           return {
-          fillColor: "#FF9900"
-        };
-      } else if (depth > 30) {
-          return {
-          fillColor: "#FFBF00"
-        };
-      } else if (depth > 10) {
-          return {
-          fillColor: "#DDFF00"
-        };
-      } else {
-          return {
-          fillColor: "#66FF00"
-        }
-      }
-    },
+              fillColor: Color(geoJsonFeature.geometry.coordinates[2]),
+              fillOpacity: 0.7,
+              weight: 0.1,
+              color: 'black'
+          }
+      },
 
-    // Set Function to display information on mouse activity
-    onEachFeature: function(feature, layer) {
+     // Set Function to display information on mouse activity
+     onEachFeature: function(feature, layer) {
 
       // Set information to be displayed - show magnitude, depth, location and url
       var popupText = "<b>Magnitude:</b> " + feature.properties.mag +
@@ -122,25 +51,106 @@ var earthquake = L.layerGroup().addTo(map);
       // Create event to display popupText information when clicking on the circle label
       layer.on('click', function() {
         layer.openPopup();
-
-        earthquake.addLayer(layer);
       });
     },
 
-    // Create function to identify earthquake location by using the latlng method
-    pointToLayer: function(feature, latlng) {
-      // Put the circle markers based on the latlng location
-      return L.circleMarker(latlng, {
-        //Set styles
-        radius: Math.round(feature.properties.mag) * 4,
-        fillOpacity: 1,
-        color: "black",
-        weight: 0.5
-      });
-    },
+    
   // Add all the layers/functions to my map to display the information
-  }).addTo(myMap);
+  }).addTo(earthquake);
+  createMap(earthquake);
 });
+
+var tectonics = new L.LayerGroup();
+
+d3.json(plateURL, function (geoJson) {
+  L.geoJSON(geoJson.features, {
+      style: function (geoJsonFeature) {
+          return {
+              weight: 2,
+              color: 'blue'
+          }
+      },
+  }).addTo(tectonics);
+})
+
+ // Set function to retrieve depth data
+function Color(depth){
+      // Conditional Statement for color scale vs depth
+      // For high depth, circles will be red and low depth, circles will be green
+      
+      if (depth >= 90.0) {
+        return "#FF3333"
+      }
+      else if (depth > 70.0) {
+        return "#FF9966"
+      } 
+      else if (depth > 50) {
+          return "#FF9900"
+      } 
+      else if (depth > 30) {
+          return "#FFBF00"
+      } 
+      else if (depth > 10) {
+          return "#DDFF00"
+      } 
+      else {
+          return "#66FF00"
+      }
+};
+   
+
+
+function createMap() {
+  // Satellite Map Style
+  var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/satellite-v9",
+    accessToken: API_KEY
+  });
+  
+  // Gray Scale Style
+  var grayscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  })
+  
+  // Outdoors Style
+  var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "outdoors-v11",
+    accessToken: API_KEY
+  });
+
+  // Create a baseMaps object
+var baseMaps = {
+  "Satellite": satelliteMap,
+  "Grayscale": grayscaleMap,
+  "Outdoors": outdoorsMap
+};
+
+// Create an overlay object
+var overlayMaps ={
+  "Earthquakes": earthquake, 
+  "Tectonics": tectonics
+};
+  
+  
+// Create a map object
+  var myMap = L.map("mapid", {
+    center: [35.222, -101.8313],
+    zoom: 5,
+    layers: [satelliteMap, earthquake, tectonics]
+  });      
+
+  L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
 
 // Set up the position of the legend
@@ -164,3 +174,14 @@ return div;
 };
 // Add legend to be displayed on myMap
 legend.addTo(myMap);
+}
+// Setting map layer style variables 
+
+
+
+
+
+
+
+
+
